8fb88a0936d700e4cbc9e505c4e6d56936d00f33
u_taku0115 <ut1995j15@gmail.com>
2023-11-11 14:23:03 +0900

    gatewayのトレイト、インタラクター追加

--- src/domain/usecase/mod.rs
+++ src/domain/usecase/mod.rs
@@ -1,6 +1,16 @@
-use super::ToDoId;
+use super::{ToDoId, model::ToDo};
 
 pub trait ToDoUseCase {
     fn create(todo: String) -> ToDoId;
-    fn delete(todo: ToDoId) -> bool;
+    fn delete(id: ToDoId) -> bool;
 }
+
+pub trait DBService {
+    fn create_todo(message: &str) -> ToDo;
+    fn delete_todo(id: ToDoId) -> bool;
+}
+
+pub struct Interactor {
+    pub todo_usecase: dyn ToDoUseCase
+}
+
