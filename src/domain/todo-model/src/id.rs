039bbd4ec7cdf8c9fd4f1a693602ee8b10c8dadc
urushi-115 <ut1995j15@gmail.com>
2024-01-02 12:14:06 +0900

    単体テスト追加

--- src/domain/todo-model/src/id.rs
+++ src/domain/todo-model/src/id.rs
@@ -2,6 +2,8 @@ use std::str::FromStr;
 
 use uuid::Uuid;
 
+use crate::ModelError;
+
 #[derive(Debug)]
 pub struct ToDoId(Uuid);
 impl ToDoId {
@@ -10,7 +12,22 @@ impl ToDoId {
     }
 }
 
-// impl FromStr for ToDoId {
-//     type Err = ;
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {}
-// }
+impl FromStr for ToDoId {
+    type Err = ModelError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let uuid = Uuid::from_str(s).map_err(|err| ModelError::Validation(format!("{:?}", err)))?;
+        Ok(Self(uuid))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ok_from_str() {
+        let s = "550e8400-e29b-41d4-a716-446655440000";
+        let id = ToDoId::from_str(s);
+        assert!(id.is_ok())
+    }
+}
