9dad40d1facbe159bcb33626a67490f33ba22c4d
urushi-115 <ut1995j15@gmail.com>
2024-01-03 10:45:10 +0900

    rdbのディレクトリを追加、db設定見直し

--- src/domain/todo-usecase/src/gateway/rdb.rs
+++ src/domain/todo-usecase/src/gateway/rdb.rs
@@ -1,4 +1,9 @@
 use async_trait::async_trait;
+use todo_model::ToDo;
+
+use crate::error::Result;
 
 #[async_trait]
-pub trait DBService {}
+pub trait DBService {
+    async fn insert_todo(&self, todo: ToDo) -> Result<()>;
+}
