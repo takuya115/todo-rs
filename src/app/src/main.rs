1de6eda338cdea002d4d4b5ac5dc9a8648325132
urushi-115 <ut1995j15@gmail.com>
2024-01-04 14:03:50 +0900

    axum導入

--- src/app/src/main.rs
+++ src/app/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = Router::new().route("/", get(root));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello World!"
 }
