b8a016bb958327ebf1c6eb97c1124f29a6d4923e
u_taku0115 <ut1995j15@gmail.com>
2024-02-03 13:32:10 +0900

    インタラクターの結合を仮実装

--- src/app/src/api/create_todo.rs
+++ src/app/src/api/create_todo.rs
@@ -1,9 +1,15 @@
-use axum::{routing::post, Router};
+use std::sync::Arc;
+
+use axum::{routing::post, Extension, Router};
+// use todo_usecase::interactor::Interactor;
+
+use crate::CustomInteractor;
 
 pub fn builder() -> Router {
     Router::new().route("/todo", post(create_todo))
 }
 
-async fn create_todo() -> String {
+async fn create_todo(Extension(modules): Extension<Arc<CustomInteractor>>) -> String {
+    println!("{}", modules.use_case());
     format!("create-todo")
 }
--- src/app/src/main.rs
+++ src/app/src/main.rs
@@ -1,16 +1,29 @@
 mod api;
 mod config;
 
-use axum::{routing::get, Router};
+use std::sync::Arc;
+
+use axum::{routing::get, Extension, Router};
 use config::Config;
+// use todo_usecase::interactor::Interactor;
+
+#[derive(Debug, Clone)]
+pub struct CustomInteractor;
+impl CustomInteractor {
+    pub fn use_case(&self) -> String {
+        "call use_case()".into()
+    }
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
     let config = Config::from_env();
+    let interactor = Arc::new(CustomInteractor);
     let app = Router::new()
         .route("/", get(root))
-        .nest("/", api::create_todo::builder());
+        .nest("/", api::create_todo::builder())
+        .layer(Extension(interactor));
     let listener = tokio::net::TcpListener::bind(&config.server_host)
         .await
         .unwrap();
