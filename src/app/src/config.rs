277ef72ba04d55333fa9996565736acb1f6caed9
urushi-115 <ut1995j15@gmail.com>
2024-05-03 14:34:56 +0900

    依存の修正

--- src/app/src/config.rs
+++ src/app/src/config.rs
@@ -9,8 +9,8 @@ pub struct Config {
 impl Config {
     pub fn from_env() -> Self {
         Self {
-            server_host: env::var("SERVER_HOST").unwrap(),
-            db_url: env::var("DATABASE_URL").unwrap(),
+            server_host: env::var("SERVER_HOST").expect("`SERVER_HOST` is not found."),
+            db_url: env::var("DATABASE_URL").expect("`DATABASE_URL` is not found."),
         }
     }
 }
